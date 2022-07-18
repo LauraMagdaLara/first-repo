29e021905bcee6fa9c29030fb320c8b0ea4f1df7
LauraMagdaLara <womanat30plus@gmail.com>
2022-07-18 15:09:53 +0300

    a doua versiune

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,6 @@
-public class HelloWord {
+public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello!");
+        System.out.println("Adauga modificare");
     }
 }
